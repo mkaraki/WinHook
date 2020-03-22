01a3b1a34c2be4fda060766568ebca3c2842b37d
mkaraki <backtrack1994+github@gmail.com>
2020-03-22 16:19:27 +0900

    Cahnge Assembly Info

--- WinHook/Properties/AssemblyInfo.cs
+++ WinHook/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WinHook")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("mkaraki apps")]
+[assembly: AssemblyCompany("mkaraki")]
 [assembly: AssemblyProduct("WinHook")]
-[assembly: AssemblyCopyright("Copyright © mkaraki apps 2020")]
+[assembly: AssemblyCopyright("Copyright © mkaraki 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
